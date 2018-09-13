ba26db88c49b49a6d8c6127daa58c1c27bacfb2b
tlebon <tlebon@gmail.com>
2018-09-13 09:59:11 +0200

    all setup done

---
+++ countries/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import './style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Your application code */}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  ),
+  document.getElementById('root')
+);
